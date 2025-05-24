63d5a4fe80d10bf078aeb2d673730550797c7322
BrevinBrown <brevinbrown@gmail.com>
2025-05-24 19:48:21 -0400

    create textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import *
+def main():
+    test = TextNode("test",TextType.NORMAL)
+    print(test.__repr__())
+    test2 = TextNode("tet",TextType.NORMAL)
+    print(test == test2)
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -10,7 +10,18 @@ class TextType(Enum):
 
 
 class TextNode():
-    def __init__(text,text_type,url=None):
+    def __init__(self,text,text_type,url=None):
         self.text = text
-        self.text_type 
+        self.text_type = text_type 
         self.url = url
+
+    def __eq__(self,node1):
+        if (node1.text == self.text and
+        node1.text_type == self.text_type and
+        node1.url == self.url):
+            return True
+        return False
+
+    def __repr__(self):
+        return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
+
